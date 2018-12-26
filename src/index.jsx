44564df3bedb29e10c5e60c0f324266a5f66d69e
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-12-26 00:21:16 -0200

    add pattern and inputMode to input

--- src/index.jsx
+++ src/index.jsx
@@ -1,167 +1,181 @@
-import React, { Component } from "react"
-import ReactDOM from "react-dom"
-import { string, func, object, number, bool } from "prop-types"
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { string, func, object, number, bool } from 'prop-types';
 
-import formatCurrency from "./format-currency"
+import formatCurrency from './format-currency';
 
 const defaultConfig = {
-  locale: "en-US",
+  locale: 'en-US',
   formats: {
     number: {
       USD: {
-        style: "currency",
-        currency: "USD",
+        style: 'currency',
+        currency: 'USD',
         minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      },
-    },
-  },
-}
+        maximumFractionDigits: 2
+      }
+    }
+  }
+};
 
 class IntlCurrencyInput extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      maskedValue: "0",
-    }
+      maskedValue: '0'
+    };
 
-    this.handleFocus.bind(this)
+    this.handleFocus.bind(this);
   }
 
   setMaskedValue(value = 0) {
     this.setState({
-      maskedValue: formatCurrency(value, this.props.config, this.props.currency),
-    })
+      maskedValue: formatCurrency(value, this.props.config, this.props.currency)
+    });
   }
 
   componentDidMount() {
-    const value = this.props.defaultValue || 0
-    this.setMaskedValue(value)
+    const value = this.props.defaultValue || 0;
+    this.setMaskedValue(value);
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.currency !== this.props.currency) {
-      const [, maskedValue] = this.calculateValues(this.state.maskedValue, nextProps.config, nextProps.currency)
-      this.setState({ maskedValue: maskedValue })
+      const [, maskedValue] = this.calculateValues(
+        this.state.maskedValue,
+        nextProps.config,
+        nextProps.currency
+      );
+      this.setState({ maskedValue: maskedValue });
     }
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return !(nextProps === this.props && nextState === this.state)
+    return !(nextProps === this.props && nextState === this.state);
   }
 
   normalizeValue(str) {
     // strips everything that is not a number (positive or negative).
-    return Number(str.replace(/[^0-9-]/g, ""))
+    return Number(str.replace(/[^0-9-]/g, ''));
   }
 
   calculateValues(inputFieldValue, config, currency) {
     // value must be divided by 100 to properly work with cents.
-    const value = this.normalizeValue(inputFieldValue) / 100
-    const maskedValue = formatCurrency(value, config, currency)
+    const value = this.normalizeValue(inputFieldValue) / 100;
+    const maskedValue = formatCurrency(value, config, currency);
 
-    return [value, maskedValue]
+    return [value, maskedValue];
   }
 
   updateValues(event) {
-    const [value, maskedValue] = this.calculateValues(event.target.value, this.props.config, this.props.currency)
+    const [value, maskedValue] = this.calculateValues(
+      event.target.value,
+      this.props.config,
+      this.props.currency
+    );
     if (!this.props.max || value <= this.props.max) {
       this.setState({
-        maskedValue,
-      })
+        maskedValue
+      });
 
-      return [value, maskedValue]
+      return [value, maskedValue];
     } else {
-      return [this.normalizeValue(this.state.maskedValue)/100, this.state.maskedValue]
+      return [
+        this.normalizeValue(this.state.maskedValue) / 100,
+        this.state.maskedValue
+      ];
     }
   }
 
   handleChange(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const [value, maskedValue] = this.updateValues(event)
+    const [value, maskedValue] = this.updateValues(event);
 
     if (this.props.onChange && maskedValue) {
-      this.props.onChange(event, value, maskedValue)
+      this.props.onChange(event, value, maskedValue);
     }
   }
 
   handleBlur(event) {
-    const [value, maskedValue] = this.updateValues(event)
+    const [value, maskedValue] = this.updateValues(event);
 
     if (this.props.autoReset) {
-      this.setMaskedValue()
+      this.setMaskedValue();
     }
 
     if (this.props.onBlur) {
-      this.props.onBlur(event, value, maskedValue)
+      this.props.onBlur(event, value, maskedValue);
     }
   }
 
   handleFocus(event) {
     if (this.props.autoSelect) {
-      event.target.select()
+      event.target.select();
     }
 
-    const [value, maskedValue] = this.updateValues(event)
+    const [value, maskedValue] = this.updateValues(event);
 
     if (this.props.onFocus) {
-      this.props.onFocus(event, value, maskedValue)
+      this.props.onFocus(event, value, maskedValue);
     }
   }
 
   handleKeyPress(event) {
     if (this.props.onKeyPress) {
-      this.props.onKeyPress(event, event.key, event.keyCode)
+      this.props.onKeyPress(event, event.key, event.keyCode);
     }
   }
 
   handleInputRef(input) {
-    const element = ReactDOM.findDOMNode(input)
-    const isActive = element === document.activeElement
+    const element = ReactDOM.findDOMNode(input);
+    const isActive = element === document.activeElement;
 
     if (element && !isActive) {
       if (this.props.autoFocus) {
-        element.focus()
+        element.focus();
       }
     }
 
-    return element
+    return element;
   }
 
   handleValue() {
-    return this.state.maskedValue
+    return this.state.maskedValue;
   }
 
   allowedProps() {
-    const allowedProps = { ...this.props }
+    const allowedProps = { ...this.props };
 
-		delete allowedProps.defaultValue
-    delete allowedProps.currency
-    delete allowedProps.config
-    delete allowedProps.autoSelect
-    delete allowedProps.autoFocus
-    delete allowedProps.autoReset
-    delete allowedProps.onChange
-    delete allowedProps.onKeyPress
-    delete allowedProps.onBlur
-    delete allowedProps.onFocus
+    delete allowedProps.defaultValue;
+    delete allowedProps.currency;
+    delete allowedProps.config;
+    delete allowedProps.autoSelect;
+    delete allowedProps.autoFocus;
+    delete allowedProps.autoReset;
+    delete allowedProps.onChange;
+    delete allowedProps.onKeyPress;
+    delete allowedProps.onBlur;
+    delete allowedProps.onFocus;
 
-    return allowedProps
+    return allowedProps;
   }
 
   render() {
     return (
-      <input {...this.allowedProps()}
+      <input
+        {...this.allowedProps()}
         value={this.handleValue()}
-        ref={input => this.input = this.handleInputRef(input)}
-        onChange={ ev => this.handleChange(ev) }
-        onBlur={ ev => this.handleBlur(ev) }
-        onFocus={ ev => this.handleFocus(ev) }
-        onKeyUp={ ev => this.handleKeyPress(ev) }
+        inputMode="numeric"
+        pattern="[0-9]*"
+        ref={input => (this.input = this.handleInputRef(input))}
+        onChange={ev => this.handleChange(ev)}
+        onBlur={ev => this.handleBlur(ev)}
+        onFocus={ev => this.handleFocus(ev)}
+        onKeyUp={ev => this.handleKeyPress(ev)}
       />
-    )
+    );
   }
 }
 
@@ -176,15 +190,15 @@ IntlCurrencyInput.propTypes = {
   onBlur: func,
   onFocus: func,
   onKeyPress: func,
-  max: number,
-}
+  max: number
+};
 
 IntlCurrencyInput.defaultProps = {
-  currency: "USD",
+  currency: 'USD',
   config: defaultConfig,
   autoFocus: false,
   autoSelect: false,
-  autoReset: false,
-}
+  autoReset: false
+};
 
-export default IntlCurrencyInput
+export default IntlCurrencyInput;
